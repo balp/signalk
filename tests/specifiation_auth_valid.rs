ce642be8e4645d1d048b1b161b82381f06322662
Anders Arnholm <anders.arnholm@hiq.se>
2024-10-17 10:53:40 +0200

    Add tests for Auth not yet implemented

---
+++ tests/specifiation_auth_valid.rs
@@ -0,0 +1,51 @@
+use signalk::full::V1FullFormat;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+/* TODO: Auth messages not supported
+#[test]
+fn login_request() {
+    let path = Path::new("tests/specification/test_data/auth-valid/login-request.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let sk_data: V1Auth = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", sk_data);
+}
+
+#[test]
+fn login_response() {
+    let path = Path::new("tests/specification/test_data/auth-valid/login-response.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let sk_data: V1Auth = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", sk_data);
+}
+
+#[test]
+fn logout_request() {
+    let path = Path::new("tests/specification/test_data/auth-valid/logout-request.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let sk_data: V1Auth = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", sk_data);
+}
+
+#[test]
+fn validate_request() {
+    let path = Path::new("tests/specification/test_data/auth-valid/validate-request.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let sk_data: V1Auth = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", sk_data);
+}
+
+#[test]
+fn validate_response() {
+    let path = Path::new("tests/specification/test_data/auth-valid/validate-response.json");
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let sk_data: V1Auth = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", sk_data);
+}
+*/
